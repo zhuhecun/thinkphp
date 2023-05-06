d3b2b158948d386dfcb41fe7deeb49ea5f01670a
歪果桃 <78370612+zhuhecun@users.noreply.github.com>
2023-05-07 06:44:02 +0800

    2023050706

--- config/database.php
+++ config/database.php
@@ -24,13 +24,13 @@ return [
             // 数据库类型
             'type'            => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'        => env('database.hostname', '127.0.0.1'),
+            'hostname'        => env('database.hostname', 'localhost'),
             // 数据库名
             'database'        => env('database.database', 's1891732'),
             // 用户名
-            'username'        => env('database.username', 'root'),
+            'username'        => env('database.username', 's1891732'),
             // 密码
-            'password'        => env('database.password', 'root'),
+            'password'        => env('database.password', 's1891732'),
             // 端口
             'hostport'        => env('database.hostport', '3306'),
             // 数据库连接参数
